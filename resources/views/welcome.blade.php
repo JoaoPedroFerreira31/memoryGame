6102fb1924601e6e357c8a6fe2a50947a46f7556
JoaoPedroFerreira <vagrant@homestead>
2021-11-25 18:29:20 +0000

    create function to display cards

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,24 +3,39 @@
 <head>
     <meta charset="UTF-8">
     <title>Document</title>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/alpinejs/2.3.0/alpine.js" integrity="sha512-nIwdJlD5/vHj23CbO2iHCXtsqzdTTx3e3uAmpTm4x2Y8xCIFyWu4cSIV8GaGe2UNVq86/1h9EgUZy7tn243qdA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
     <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
 
 </head>
 <body>
 
-    <div class="px-10">
-        <div class="grid grid-cols-4 gap-10">
-            <div class="h-32 bg-gray-300"></div>
-            <div class="h-32 bg-gray-300"></div>
-            <div class="h-32 bg-gray-300"></div>
-            <div class="h-32 bg-gray-300"></div>
-            <div class="h-32 bg-gray-300"></div>
-            <div class="h-32 bg-gray-300"></div>
-            <div class="h-32 bg-gray-300"></div>
-            <div class="h-32 bg-gray-300"></div>
+    <div x-data="game()" class="px-10 flex items-center justify-center min-h-screen">
+        <div class="flex-1 grid grid-cols-4 gap-10">
+            <template x-for="card in cards">
+                <div :style=" 'background: ' + (card.flipped ? card.color : '#999')" @click="card.flipped = ! card.flipped" class="h-32 cursor-pointer"></div>
+            </template>
         </div>
     </div>
+
+    <script>
+
+        //function to generate cards
+        function game() {
+            return {
+                cards: [
+                    {color: 'green', flipped: false, cleared: false}, //generate card
+                    {color: 'red', flipped: false, cleared: false},
+                    {color: 'blue', flipped: false, cleared: false},
+                    {color: 'yellow', flipped: false, cleared: false},
+                    {color: 'green', flipped: false, cleared: false},
+                    {color: 'red', flipped: false, cleared: false},
+                    {color: 'blue', flipped: false, cleared: false},
+                    {color: 'yellow', flipped: false, cleared: false},
+                ]
+            };
+        }
+
+    </script>
 
 </body>
 </html>

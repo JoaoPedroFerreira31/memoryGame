9df170da734565535937fe6caae1ce7ef28ea736
JoaoPedroFerreira <vagrant@homestead>
2021-11-25 16:18:52 +0000

    first-commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/alpinejs/2.3.0/alpine.js" integrity="sha512-nIwdJlD5/vHj23CbO2iHCXtsqzdTTx3e3uAmpTm4x2Y8xCIFyWu4cSIV8GaGe2UNVq86/1h9EgUZy7tn243qdA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
+
+</head>
+<body>
+
+    <div class="px-10">
+        <div class="grid grid-cols-4 gap-10">
+            <div class="h-32 bg-gray-300"></div>
+            <div class="h-32 bg-gray-300"></div>
+            <div class="h-32 bg-gray-300"></div>
+            <div class="h-32 bg-gray-300"></div>
+            <div class="h-32 bg-gray-300"></div>
+            <div class="h-32 bg-gray-300"></div>
+            <div class="h-32 bg-gray-300"></div>
+            <div class="h-32 bg-gray-300"></div>
+        </div>
+    </div>
+
+</body>
+</html>
